d9be8b14ab157ee0e778849817058bacec62a957
zss66 <zjb520zll@gmail.com>
2024-03-21 14:37:38 +0800

    后台页面

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .footer {
+    height: 50px;
+    border-top: 1px solid #e9e9e9;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+  }
+</style>
